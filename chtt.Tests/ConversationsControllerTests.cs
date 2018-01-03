4841b3388954173e6a502ad6b8ccc7844329a135
Mikhail Paulyshka <me@mixaill.tk>
2018-01-03 04:02:11 +0300

    chtt.Tests/Conversations: add some more tests

--- chtt.Tests/ConversationsControllerTests.cs
+++ chtt.Tests/ConversationsControllerTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -27,17 +28,21 @@ namespace chtt.Tests
             InitHttpContext();
         }
 
-        private chttContext _context;
+        private static Random random = new Random();
+        private static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
         private User _user;
-        private Mock<UserManager<User>> _userManager;
-        private Mock<HttpContext> _httpContext;
-        private ConversationsController _controller;
 
         #region Initialization
-        private void InitContext()
+        private chttContext InitContext()
         {
             var builder = new DbContextOptionsBuilder<chttContext>()
-                .UseInMemoryDatabase()
+                .UseInMemoryDatabase(RandomString(10))
                 .EnableSensitiveDataLogging();
 
             var context = new chttContext(builder.Options);
@@ -50,14 +55,31 @@ namespace chtt.Tests
             };
             context.User.Add(_user);
 
+            var _otherUser = new User
+            {
+                UserName = "Test2",
+                Id = Guid.NewGuid().ToString(),
+                Email = "test2@test.it"
+            };
+            context.User.Add(_otherUser);
+
             var _conversation = new Conversation
             {
-                ConversationId = 1,
+                ConversationId = 2,
                 Author = _user,
                 Description = "test description",
                 Name = "test name"
             };
             context.Conversation.Add(_conversation);
+
+            var _otherConversation = new Conversation
+            {
+                ConversationId = 3,
+                Author = _otherUser,
+                Description = "test description2",
+                Name = "test name2"
+            };
+            context.Conversation.Add(_otherConversation);
 
             context.ConversationUser.Add(new ConversationUser
             {
@@ -66,39 +88,48 @@ namespace chtt.Tests
             });
 
             context.SaveChanges();
-
-            _context = context;
+          
+            return context;
         }
 
-        private void InitUserManager()
+        private Mock<UserManager<User>> InitUserManager()
         {
             var userStoreMock = new Mock<IUserStore<User>>();
-            _userManager = new Mock<UserManager<User>>(userStoreMock.Object, null, null, null, null, null, null, null, null);
+            var _userManager = new Mock<UserManager<User>>(userStoreMock.Object, null, null, null, null, null, null, null, null);
 
             _userManager
                 .Setup(mgr => mgr.FindByNameAsync(It.IsAny<string>()))
                 .Returns(Task.FromResult(_user));
-
+            return _userManager;
         }
 
-        private void InitHttpContext()
+        private Mock<HttpContext> InitHttpContext()
         {
-            _httpContext = new Mock<HttpContext>();
+            var _httpContext = new Mock<HttpContext>();
             _httpContext
                 .Setup(x => x.User)
                 .Returns(new ClaimsPrincipal());
+            return _httpContext;
         }
+
+        private ConversationsController GetConversationController(bool setHttpContext = true)
+        {
+            var controller = new ConversationsController(InitContext(), InitUserManager().Object);
+            if (setHttpContext)
+            {
+                controller.ControllerContext.HttpContext = InitHttpContext().Object;
+            }
+
+            return controller;
+        }
+        
         #endregion
 
 
         [Fact]
         public void GetConversations_User()
         {
-            var controller = new ConversationsController(_context, _userManager.Object)
-                {
-                    ControllerContext = { HttpContext = _httpContext.Object }
-                };
-
+            var controller = GetConversationController();
             var res = controller.GetConversations().Result as OkObjectResult;
             Assert.NotNull(res);
             Assert.Equal(200, res.StatusCode);
@@ -111,10 +142,93 @@ namespace chtt.Tests
         [Fact]
         public void GetConversations_NoUser()
         {
-            var controller = new ConversationsController(_context, _userManager.Object);
+            var controller = GetConversationController(false);
             var res = controller.GetConversations().Result as UnauthorizedResult;
             Assert.Equal(401, res.StatusCode);
         }
+
+        [Fact]
+        public void GetConversation_Exists()
+        {
+            var controller = GetConversationController();
+
+            var res = controller.GetConversation(2).Result as OkObjectResult;
+            Assert.NotNull(res);
+            Assert.Equal(200, res.StatusCode);
+
+            var value = res.Value as GetViewModel;
+            Assert.NotNull(value);
+            Assert.Equal("test name", value.Name);
+            Assert.Equal(_user.UserName,value.Author);
+        }
+
+        [Fact]
+        public void GetConversation_NonExists()
+        {
+            var controller = GetConversationController();
+
+            var res = controller.GetConversation(100500).Result as NotFoundResult;
+            Assert.NotNull(res);
+            Assert.Equal(404, res.StatusCode);
+        }
+
+        [Fact]
+        public void GetConversation_Forbidden()
+        {
+            var controller = GetConversationController();
+
+            var res = controller.GetConversation(3).Result as ForbidResult;
+            Assert.NotNull(res);
+        }
+
+        [Fact]
+        public void PostConversation()
+        {
+            var controller = GetConversationController();
+
+            var c = new CreateViewModel
+            {
+                Name = "testname_post",
+                Description = "testdescr_post"
+            };
+            var res = controller.PostConversation(c).Result;
+            Assert.NotNull(res);
+        }
+
+        [Fact]
+        public void PutConversation_Valid()
+        {
+            var controller = GetConversationController();
+
+            var u = new UpdateViewModel
+            {
+                ConversationId = 2,
+                Description = "test description",
+                Name = "test name",
+                Users = new List<string>() { "Test" }
+            };
+            var res = controller.PutConversation(2, u).Result as NoContentResult;
+            Assert.NotNull(res);
+            Assert.Equal(204,res.StatusCode);
+        }
+
+        [Fact]
+        public void PutConversation_InvalidID()
+        {
+            var controller = GetConversationController();
+
+            var u = new UpdateViewModel
+            {
+                ConversationId = 22,
+                Description = "test description",
+                Name = "test name",
+                Users = new List<string>() { "Test" }
+            };
+            var res = controller.PutConversation(2, u).Result as BadRequestResult;
+            Assert.NotNull(res);
+            Assert.Equal(400, res.StatusCode);
+        }
+
 
     }
 }
